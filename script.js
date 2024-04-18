ad8f50751e1d561ab9ce2d7718f134719e0bde3c
marcPC <marcnelpanganiban0808@gmail.com>
2024-04-18 22:09:45 +0800

    add music and picture

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to redirect to another HTML file when button is clicked
+document.getElementById("button_total").onclick = function () {
+    location.href = "content.html"; // Change the filename to the one you want to navigate to
+};
